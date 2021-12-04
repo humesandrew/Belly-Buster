ec970607208532159ee9f50f335f3e093e36c067
Andrew Nguyen <blueandyn@gmail.com>
2021-12-04 13:21:57 -0700

    map changes

--- script.js
+++ script.js
@@ -67,6 +67,17 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
 function displayMap () {
     var getMap = document.getElementById("map");
    
